70991485c37d174d2a4f495f27634816f16466d4
Nicolas Henrique <nicolashenrique1402@gmail.com>
2026-05-20 10:51:06 -0300

    Primeiro commit

---
+++ visual code.py
@@ -0,0 +1,6 @@
+usuario = input('Digite seu usuario: ')
+senha = input('Digite sua senha: ')
+if usuario == 'admin' and senha == 'fatec123':
+    print ('acesso permitido')
+else:
+    print ('acesso negado')
